1ccae3f55842b2ef91d5b47eb064ee2085db1d28
tom5265 <tom5265@gmail.com>
2016-04-11 15:36:21 -0500

    fixed drawer close

--- app/src/templatecontrols/maindrawer/maindrawer.tc.ts
+++ app/src/templatecontrols/maindrawer/maindrawer.tc.ts
@@ -2,6 +2,19 @@ import {register, ui} from 'platypus';
 
 export default class MaindrawerTemplateControl extends ui.TemplateControl {
     templateString: string = require('./maindrawer.tc.html');
+    hasOwnContext = true;
+    
+    context = {
+        showDrawer: false
+    }
+    
+    closeDrawer() {
+        this.context.showDrawer = false;
+    }
+    
+    setTemplate() {
+        this.addEventListener(this.element, '$tap', this.closeDrawer);
+    }
 }
 
 register.control('maindrawer', MaindrawerTemplateControl);
