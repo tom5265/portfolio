eb6cb40c0a1f234e8ed3d35171b194748776478e
tom5265 <tommoore@Toms-MacBook-Pro.local>
2016-05-13 14:56:56 -0500

    updated hero photo and fix for 100vh size

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -5,58 +5,59 @@ export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
 
     context: any = {
- 
-    };
- 
-   rollScroll() {  let scroll = (function() {
 
-    let elementPosition = function(a:any) {
-        return function() {
-            return a.getBoundingClientRect().top;
-        };
     };
 
-    let scrolling = function( elementID: any ) {
+    rollScroll() {
+        let scroll = (function () {
 
-        let el:any = document.getElementById( elementID ),
-            elPos = elementPosition( el ),
-            duration = 200,
-            increment = Math.round( Math.abs( elPos() )/40 ),
-            time = Math.round( duration/increment ),
-            prev = 0,
-            E:any;
+            let elementPosition = function (a: any) {
+                return function () {
+                    return a.getBoundingClientRect().top;
+                };
+            };
 
-        function scroller() {
-            E = elPos();
-            
-            if (E === prev) {
-                return;
-            } else {
-                prev = E;
-            }
+            let scrolling = function (elementID: any) {
 
-            increment = (E > -20 && E < 20) ? ((E > - 5 && E < 5) ? 1 : 5) : increment;
-            if (E > 1 || E < -1) {
-                if (E < 0) {
-                    window.scrollBy( 0,-increment );
-                } else {
-                    window.scrollBy( 0,increment );
+                let el: any = document.getElementById(elementID),
+                    elPos = elementPosition(el),
+                    duration = 200,
+                    increment = Math.round(Math.abs(elPos()) / 40),
+                    time = Math.round(duration / increment),
+                    prev = 0,
+                    E: any;
+
+                function scroller() {
+                    E = elPos();
+
+                    if (E === prev) {
+                        return;
+                    } else {
+                        prev = E;
+                    }
+
+                    increment = (E > -20 && E < 20) ? ((E > - 5 && E < 5) ? 1 : 5) : increment;
+                    if (E > 1 || E < -1) {
+                        if (E < 0) {
+                            window.scrollBy(0, -increment);
+                        } else {
+                            window.scrollBy(0, increment);
+                        }
+                        setTimeout(scroller, time);
+                    } else {
+                        el.scrollTo(0, 0);
+                    }
                 }
-                setTimeout(scroller, time);
-            } else {
-                el.scrollTo( 0,0 );
-            }
-        }
-        scroller();
-    };
+                scroller();
+            };
 
-    return {
-        To: scrolling
+            return {
+                To: scrolling
+            }
+        })();
+        scroll.To('scroll-div');
     }
-})();
-scroll.To('educ-link');
-   }
-   
+
 
 }
 register.viewControl('home-vc', HomeViewControl);
